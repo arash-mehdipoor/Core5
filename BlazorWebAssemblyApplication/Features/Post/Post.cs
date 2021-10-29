b7ad2617d70dfc86cd2556e091a96a436375c9b8
Arash Mehdipour <arash201221@gmail.com>
2021-10-29 13:24:52 +0330

    SearchBlazor

--- BlazorWebAssemblyApplication/Features/Post/Post.cs
+++ BlazorWebAssemblyApplication/Features/Post/Post.cs
@@ -11,5 +11,6 @@ namespace BlazorWebAssemblyApplication.Features.Post
         public string Title { get; set; }
         public string Description { get; set; }
         public string Url { get; set; }
+        public DateTime PublishDate { get; set; }
     }
 }
